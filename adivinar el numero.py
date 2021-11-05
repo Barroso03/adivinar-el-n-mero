8934ef8416ad293d383f0a84698ef697139c5372
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-11-05 19:43:35 +0100

    nuevos cambios

--- adivinar el numero.py
+++ adivinar el numero.py
@@ -11,13 +11,21 @@ while True:
         if 0 <= numero <= 99:
 print("intente encontrar el número a adivinar")
 while True:
-    intento = input("Introduzca un número entre 0 y 99 incluidos:")
+    numero = input("Introduzca el número a adivinar") 
     try:
-        intento = int(intento)
+        numero = int(numero)
     except:
         pass
     else:
-        if 0 <= intento <= 99:
+        if 0 <= numero <= 99:
+    while True:
+        intento = input("Introduzca un número entre 0 y 99 incluidos:")
+        try:
+            numero = int(intento)
+        except:
+            pass
+        else:
+            if 0 <= intento <= 99:
 if intento < numero:
    print("Demasiado pequeño")
 elif intento > numero:
