2f20962f31243b5914283131f99a3964f0b82da4
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-11-14 21:52:21 +0100

    definimos pedir entrada del numero incognita

--- adivinar el numero funciones.py
+++ adivinar el numero funciones.py
@@ -1,3 +1,16 @@
 from entrada import (pedir_entrada_numero, pedir_entrada_numero_delimitado, pedir_entrada_si_o_no)
 def jugar_una_vez(numero,minimo,maximo):
     intento = pedir_entrada_numero_delimitado("Adivine el numero", minimo, maximo)
+    if intento < numero:
+        print("Demasiado pequeño")
+        minimo = intento + 1
+        victoria = False
+    elif intento > numero:
+        print("Demasiado grande")
+        maximo = intento - 1
+        victoria = False
+    else:
+        print("¡Ha ganado!")
+def pedir_entrada_del_numero_incognita(minimo,maximo):
+    return pedir_entrada_numero_delimitado("Introduzca el numero a adivinar", minimo, maximo)
+
